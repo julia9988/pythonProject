70c01800364c931a0d74bb701acdd3a49b699cf7
julia9988 <liubetska@gmail.com>
2022-01-24 16:28:06 +0200

    "hometask1_2"

---
+++ lesson1_2.py
@@ -0,0 +1,54 @@
+#Python_2. HW_1
+#1) Создать переменную типа String
+a = "Serg"
+print(a)
+print(type(a))
+#2) Создать переменную типа Integer
+b = 1
+print(b)
+print(type(b))
+#3) Создать переменную типа Float
+c = 1.22
+print(c)
+print(type(c))
+#4) Создать переменную типа Bytes
+d = bytes(22)
+print(d)
+print(type(d))
+#5) Создать переменную типа List
+e = [1111, 1.2, "asdf", 233445]
+print(e)
+print(type(e))
+#6) Создать переменную типа Tuple
+f = (77, 88, 55, 3)
+print(f)
+print(type(f))
+#7) Создать переменную типа Set
+g = {1, 44, 6, 7}
+print(g)
+print(type(g))
+#8. Создать переменную типа Frozen set
+h = {"apple", "banana", "cherry"}
+print(h)
+print(type(h))
+#9) Создать переменную типа Dict
+i = {
+    "animal": "cat",
+    "gen": "male",
+    "age": 3,
+}
+print(i)
+print(type(i))
+#10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных.
+#11) Создать 2 переменные String, создать переменную в которой сканкатенируете эти переменные. Вывести в консоль.
+A1 = "He is a "
+A2 = "doctor"
+print(A1 + A2)
+#12) Вывести в одну строку переменные типа String и Integer используя “,” (Запятую)
+A3 = "We are 98"
+A4 = str(99)
+print(", ".join([A3, A4]))
+#13) Вывести в одну строку переменные типа String и Integer используя “+” (Плюс)
+B1 = str(6)
+B2 = "doctor"
+print(B1 + B2)
